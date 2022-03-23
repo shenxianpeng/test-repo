021b83e0dd32f770df227b607143fbfccdd49e6e
Peter Shen <xianpeng.shen@gmail.com>
2022-03-23 19:57:07 +0800

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+/** This is a very ugly test code (doomed to fail linting) */
+#include "demo.hpp"
+#include <cstdio>
+
+int main()
+{
+    for (;;)
+        break;
+
+    printf("Hello world!\n");
+
+    return 0;
+}
+
---
+++ demo.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+
+
+class Dummy {
+    char* useless;
+    int numb;
+
+    public:
+    void *not_usefull(char *str){
+        useless = str;
+        return 0;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct LongDiff
+{
+    long diff;
+
+};
